76ad560270b1fb22cff8f00ac4a06debb9dc3e13
Corentin <corentin.zheng@gmail.com>
2019-10-14 14:05:56 +0200

    Try to register favorites

--- front/src/components/Tree.vue
+++ front/src/components/Tree.vue
@@ -2,16 +2,16 @@
     <v-container>
         <v-layout v-if="tree !== null">
             <v-flex xs6>
-                <v-card color="#C5E1A5"  class="mx-auto">
+                <v-card color="#C5E1A5"  class="mx-auto" v-model="name">
                     <v-card-title>{{ tree.genus }} {{ tree.specie }} {{ tree.variety }}</v-card-title>
                     <v-card-text>{{ tree.location }}</v-card-text>
                     <v-card-text>Hauteur : {{ tree.height }} m</v-card-text>
                     <v-col class="text-right">
-                        <v-btn v-model="fav"
+                        <v-btn
                         text icon 
                         :color="color" 
-                        @click="change_color"
-                        >{{ fav }}
+                        @click.once="change_color"
+                        >
                         <v-icon>mdi-heart</v-icon>
                         </v-btn>
                     </v-col>
@@ -26,11 +26,15 @@
         props: ['tree'],
         data: () => ({
             color: 'grey',
-            fav: []
+            fav: [],
+            name: ''
         }),
         methods: {
             change_color () {  
-                this.color = "pink"            
+                this.color = "pink",
+                
+                this.fav.push(this.name)
+                console.log("Ajout d'un favori")
             },            
         }
     };
