eedec8b1945c0cb0b52e1d494fb9f29849014a31
Corentin <corentin.zheng@gmail.com>
2019-10-15 10:28:07 +0200

    Login works

--- front/src/components/Login.vue
+++ front/src/components/Login.vue
@@ -22,14 +22,16 @@
       </v-card-text>
       <v-divider></v-divider>
       <v-card-actions>
-        <v-btn color="success" @click="register">Register</v-btn>
-        <v-btn color="info" @click="login">Login</v-btn>
+        <v-btn color="success" @click='register'>Register</v-btn>
+        <v-btn color="info" @click='login'>Login</v-btn>
       </v-card-actions>
     </v-card>
   </v-app>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data: () => ({
     showPassword: false,
@@ -41,21 +43,22 @@ export default {
       axios.get("http://localhost:8000/api/v1/trees").then(response => {
         this.trees = response.data;
       });
-    }
-  },
+    },
   register() {
-    let param = {
-      username: this.username,
-      password: this.password
-    };
-    axios.post("http://localhost:8000/api/v1/user", param).then(response => {
-      this.user = response.data;
-    });
-    if (this.$refs.form.validate()) {
-      console.log("User validated !");
-    }
-  },
+  let param = {
+    username: this.username,
+    password: this.password
+  };
+  axios.post("http://localhost:8000/api/v1/user", param).then(response => {
+    this.user = response.data;
+  });
+  if (this.$refs.form.validate()) {
+    console.log("User validated !");
+  }
+},
+  login() {
 
-  login() {}
+  }
+  }
 };
 </script>
