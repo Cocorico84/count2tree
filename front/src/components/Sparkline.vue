7001951739d53e34cd6b354ccbf320783b791aac
Corentin <corentin.zheng@gmail.com>
2019-10-15 16:11:03 +0200

    Sparkline change

--- front/src/components/Sparkline.vue
+++ front/src/components/Sparkline.vue
@@ -1,17 +1,20 @@
 <template>
-  <v-sparkline
-    :value="value"
-    :gradient="gradients"
-    :smooth="radius || false"
-    :padding="padding"
-    :line-width="width"
-    :stroke-linecap="lineCap"
-    :gradient-direction="gradientDirection"
-    :fill="fill"
-    :type="type"
-    :auto-line-width="autoLineWidth"
-    auto-draw
-  ></v-sparkline>
+  <v-sheet>
+    <v-sparkline
+      :value="value"
+      :gradient="gradients"
+      :smooth="radius || false"
+      :padding="padding"
+      :line-width="width"
+      :stroke-linecap="lineCap"
+      :gradient-direction="gradientDirection"
+      :fill="fill"
+      :type="type"
+      :auto-line-width="autoLineWidth"
+      auto-draw
+      :labels="value"
+    ></v-sparkline>
+  </v-sheet>
 </template>
 
 <script>
